bdfbf7e3c7c70319a6dc55faeedb47e95fa50dc9
203459 <203459@ids.upchiapas.edu.mx>
2023-02-03 23:59:54 -0600

    first commit

---
+++ lib/components/contents_onboard.dart
@@ -0,0 +1,33 @@
+
+
+List<Map<String, String>>contents = [
+ {
+      "titulo": "ESPARCIMIENTO",
+      "imagen": "assets/images/B1.png",
+      "descripcion": "Brindamos todos los servicios para consentir a tu mascota"
+    },
+    {
+      "titulo": "ADOPCION",
+      "imagen": "assets/images/B2.png",
+      "descripcion":
+          "Nulla faucibus tellus ut odio scelerisque vitae molestie lectus feugiat."
+    },
+    {
+      "titulo": "HOSPITALIDAD",
+      "imagen": "assets/images/B3.png",
+      "descripcion":
+          "Nulla faucibus tellus ut odio scelerisque vitae molestie lectus feugiat."
+    },
+    {
+      "titulo": "VETERINARIA",
+      "imagen": "assets/images/B4.png",
+      "descripcion":
+          "Nulla faucibus tellus ut odio scelerisque vitae molestie lectus feugiat."
+    },
+    {
+      "titulo": "TIENDA",
+      "imagen": "assets/images/B5.png",
+      "descripcion":
+          "Compra todas las necesidades de tu mascota sin salir de casa"
+    },
+  ];
---
+++ lib/screens/body_boarding.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/contents_onboard.dart';
+import '../components/on_boarding_slide.dart';
+import 'home_page.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentIndex = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(children: [
+        Expanded(
+          flex: 20,
+          child: PageView.builder(
+            onPageChanged: (value) => {
+              setState(() {
+                currentIndex = value;
+              })
+            },
+            itemBuilder: (context, index) => OnBoardingSlide(
+              imagen: contents[index]["imagen"],
+              titulo: contents[index]["titulo"],
+              descripcion: contents[index]["descripcion"],
+            ),
+            itemCount: contents.length,
+            controller:_controller,
+          ),
+        ),
+        Expanded(
+          flex: 6,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  contents.length,
+                  (index) => buildPage(index: index, currentePage: currentIndex),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: SizedBox(
+                  width: 350,
+                  height: 60,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      currentIndex == contents.length - 1
+                          ? Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                            )
+                          :  _controller.nextPage(
+                              curve: Curves.easeInSine,
+                              duration: const Duration(milliseconds: 500));
+                    },
+                    style: OutlinedButton.styleFrom(
+                        backgroundColor: currentIndex == contents.length - 1
+                            ? const Color.fromARGB(255, 136, 201, 45)
+                            : const Color.fromARGB(255, 255, 255, 255),
+                        side: BorderSide(
+                            width: 2.0,
+                            color: currentIndex == contents.length - 1
+                                ? const Color.fromARGB(255, 136, 201, 45)
+                                : const Color.fromARGB(193, 105, 81, 81)),
+                        textStyle: const TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(28.0),
+                        )),
+                    child: Text(
+                      "Siguiente",
+                      style: TextStyle(
+                        color: currentIndex == contents.length - 1
+                            ? const Color.fromARGB(255, 255, 255, 255)
+                            : const Color.fromARGB(193, 105, 81, 81),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+  Container buildPage({required int index, required int currentePage}) {
+    return Container(
+     margin: const EdgeInsets.only(right: 10),
+    width: currentePage == index ? 30 : 20,
+
+    height: 5,
+    decoration: BoxDecoration(
+        color: currentePage == index
+            ? const Color.fromARGB(255, 255, 35, 35)
+            : const Color.fromARGB(255, 201, 182, 182),
+        borderRadius: BorderRadius.circular(20)),
+  );
+}
